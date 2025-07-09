4d4465a6766de40e70b0265a4fbf9802fae24287
Aspa <kreoll@gmail.com>
2025-07-09 18:20:57 +0300

    schema fix

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,11 +1,11 @@
-INSERT INTO MPA_RATING(ID, NAME) VALUES
+MERGE INTO MPA_RATING(ID, NAME) VALUES
     (1, 'G'),
     (2, 'PG'),
     (3, 'PG-13'),
     (4, 'R'),
     (5, 'NC-17');
 
-INSERT INTO GENRE(ID, NAME) VALUES
+MERGE INTO GENRE(ID, NAME) VALUES
     (1, 'Комедия'),
     (2, 'Драма'),
     (3, 'Мультфильм'),
