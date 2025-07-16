5c840c8d60bf77c5fe001372f87b4575a25d50ff
ksamylin <kirill-itb@yandex.ri>
2025-07-16 15:31:46 +0300

    13 спринт. - Функциональность «Общие фильмы». 1 SP

--- src/main/java/ru/yandex/practicum/filmorate/data/service/FilmService.java
+++ src/main/java/ru/yandex/practicum/filmorate/data/service/FilmService.java
@@ -175,4 +175,23 @@ public class FilmService {
                 .collect(Collectors.toList());
     }
 
+    public List<FilmDto> getCommonFilms(Long userId, Long friendId) throws NotFoundException, ConditionsException {
+        log.info("Общие фильмы пользователей (старт). пользователь 1: {} пользователь 2: {}", userId, friendId);
+        repositoryUser.findByIdOrThrow(userId);
+        repositoryUser.findByIdOrThrow(friendId);
+
+        if (Objects.equals(userId, friendId)) {
+            throw new ConditionsException("Нельзя искать общие фильмы у одного пользователя");
+        }
+
+        log.info("Общие фильмы пользователей (стоп). пользователь 1: {} пользователь 2: {}", userId, friendId);
+
+        List<Film> films =  repository.getCommonFilms(userId, friendId);
+
+        return films.stream()
+                .distinct()
+                .map(mapper::toDto)
+                .collect(Collectors.toList());
+    }
+
 }
